a7284ba08252d293266989d878decdf1c926d078
Wojciech Kucharski <wojtekkucharski105@gmail.com>
2021-03-17 22:07:20 +0100

    FC

--- generator.py
+++ generator.py
@@ -12,29 +12,23 @@ def random(size = 1, simple = False, use_time = True):
     else:
         return r2(correct(size), c = x0)
 
-def r1(size = 5, x0 = math.pi, z = math.pi**3, loopBreak = False):
+def r1(size = 5, x0 = math.pi, z = math.pi**3):
     tab = [x0 % 1]
     for x in range(size + 10):
         tab.append(tab[-1]*z - math.floor(tab[-1]*z))
-        if loopBreak and x > 2:
-            if tab[1] == tab[-1]:
-                return len(tab)
+
     if size == 1:
         return tab[-1]
     return tab[-size:]
 
-def r2(size = 5, c = math.pi, k = 10, loopBreak = False):
+def r2(size = 5, c = math.pi, k = 10):
     a = r1(k)
     tab = [c % 1]
     for x in range(size + 10):
         new = 0.0
         for i in range(min(x + 1, k)):
             new += tab[-i-1] * a[i]
-        new = (new+c) % 1
-        if loopBreak and x > 2:
-            if new == tab[1]:
-                return len(tab)
-        tab.append(new)
+        tab.append((new+c) % 1)
     if size == 1:
         return tab[-1]
     return tab[-size:]
