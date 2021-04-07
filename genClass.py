7e9afe3410a383bd22fd31f46f3ac8271ea0a0ce
Wojciech Kucharski <wojtekkucharski105@gmail.com>
2021-04-07 14:52:19 +0200

    FC

--- genClass.py
+++ genClass.py
@@ -2,23 +2,6 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import random as randLib
-import struct
-def binary(num):
-    return ''.join(bin(ord(c)).replace('0b', '').rjust(8, '0') for c in struct.pack('!f', num))
-
-def histArr(X, nbins = 10, var = False):
-    Xmin, Xmax = np.min(X), np.max(X)
-    xaxis = np.linspace(Xmin, Xmax, nbins + 1)
-    a, b = np.histogram(X, xaxis)
-    if var:
-        return np.var(nbins * a / len(X))
-    return nbins * a / len(X)
-
-def Funkcja_zadanie_2(x):
-    if x < 1/100:
-        return 50
-    else:
-        return 50/99
 
 class generator:
     def __init__(self, type = 1, x0 = math.pi, zk = math.pi ** 3, distr = 0):
@@ -140,6 +123,51 @@ class reject2:
         else:
             return z
 
+class numeric:
+    def __init__(self, a, b, N, fun):
+        self.N = N
+        self.calculate(fun, a, b)
+
+    def rand(self, size):
+        output = []
+        for i in range(size):
+            output.append(
+                self.reversed[math.floor(self.N * randLib.random())]
+            )
+        return output
+
+    def calculate(self, fun, a, b):
+        t = np.linspace(a, b, self.N) #OX dystrybuanty
+        distr = [0] #Oy dystrybuanty
+        for i in range(1, self.N):
+            x = a + i * (b - a) / self.N
+            distr.append(distr[-1] + eval(fun))
+        for i in range(len(distr)):
+            distr[i] /= max(distr) #normalizacja
+
+        maxj = 0
+        g = np.linspace(0, 1, self.N)
+        for i in range(self.N):
+            for j in range(max(0, maxj - 2), self.N - 1):
+                if g[i] > distr[j] and g[i] <= distr[j + 1]:
+                    maxj = j
+                    g[i] = (t[j] + t[j + 1])/2
+                    break
+        g[0] = g[1]
+        self.distr = distr
+        self.reversed = g
+
+
+def unit(t):
+    return float(t>=0)
+
+
+def discrete(x, p = [1, 2, 3, 4, 5, 6],
+             v = [1/6, 1/6, 1/6, 1/6, 1/6, 1/6]):
+    o = 0
+    for i in range(min(len(p), len(v))):
+        o += unit(x - sum(p[:i])) * (v[i] - v[i-1] * (i!=0))
+    return o
 
 def analyse(data, nbins = 100):
     output = np.zeros(nbins)
@@ -195,14 +223,7 @@ def autocorr(data, dt, m = None, v = None, printprogress = False):
             print(f"{len(output)}/{len(dt)}")
     return output
 
-
-def dual(data):
-    for i in range(len(data) - 1):
-        data[i] = data[i+1] * data[i]
-    return data[:-1]
-
-
-def multi_hist(z = [1.1, 2.2, 3.3]):
+def multi_hist(z=[1.1, 2.2, 3.3]):
     r = generator(type=0)
     fig, axs = plt.subplots(len(z))
     for i in range(len(z)):
@@ -211,40 +232,10 @@ def multi_hist(z = [1.1, 2.2, 3.3]):
         axs[i].set_title(f"z = {z[i]}")
     plt.show()
 
-
-
-class numeric:
-    def __init__(self, a, b, N, fun):
-        self.N = N
-        self.calculate(fun, a, b)
-
-    def rand(self, size):
-        output = []
-        for i in range(size):
-            output.append(
-                self.reversed[math.floor(self.N * randLib.random())]
-            )
-        return output
-
-    def calculate(self, fun, a, b):
-        t = np.linspace(a, b, self.N) #OX dystrybuanty
-        distr = [0] #Oy dystrybuanty
-        for i in range(1, self.N):
-            x = a + i * (b - a) / self.N
-            distr.append(distr[-1] + eval(fun))
-        for i in range(len(distr)):
-            distr[i] /= max(distr) #normalizacja
-
-        maxj = 0
-        g = np.linspace(0, 1, self.N)
-        for i in range(self.N):
-            for j in range(max(0, maxj - 2), self.N - 1):
-                if g[i] > distr[j] and g[i] <= distr[j + 1]:
-                    maxj = j
-                    g[i] = (t[j] + t[j + 1])/2
-                    break
-        g[0] = g[1]
-        self.distr = distr
-        self.reversed = g
-
-
+def histArr(X, nbins = 10, var = False):
+    Xmin, Xmax = np.min(X), np.max(X)
+    xaxis = np.linspace(Xmin, Xmax, nbins + 1)
+    a, b = np.histogram(X, xaxis)
+    if var:
+        return np.var(nbins * a / len(X))
+    return nbins * a / len(X)
