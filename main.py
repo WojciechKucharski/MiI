9843a7bba92d11b3461f3869866b7dd07fce9a56
Wojciech Kucharski <wojtekkucharski105@gmail.com>
2021-05-06 19:31:45 +0200

    FC

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import math
 import random
 import time
 from estimators import *
+
+gen = normal_generator(1, 1)
+f = Tester(gen)
+x = np.linspace(-1, 3, 100)
+plt.plot(f.fN(N=100, X=x, kernel="float(3/4*(1-x**2)) * float(x<=1 and x>=-1)", hN=1))
+plt.show()
