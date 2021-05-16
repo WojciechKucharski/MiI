0c1fda6eb51623665a536ec79cf26e9c7079f1ec
Wojciech Kucharski <wojtekkucharski105@gmail.com>
2021-05-16 22:50:52 +0200

    FC

--- main.py
+++ main.py
@@ -3,13 +3,37 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-s = StaticSystem(sigma=0.1, a=1)
 
 
-x = np.linspace(-math.pi, math.pi, 1000)
-Xn, Zn, Yn = s.simulate(1000)
-plt.plot(x, [s.m(xn) for xn in x])
-plt.plot(x, s.mN(100, 10, x))
-plt.show()
+def Lab7_zad2(N: int, sigma: float = 0.1, type: str = "N"):
+    s = StaticSystem(sigma=sigma, distr=type)
+    Xn, Zn, Yn = s.simulate(N)
+    plt.scatter(Xn, Yn)
+    plt.title("Chmura pomiarów")
+    plt.ylabel("Yn")
+    plt.xlabel("Xn")
+    plt.show()
+
+def Lab7_zad4(N: int, M: int, L: int, sigma: float = 0.1, type: str = "N"):
+    s = StaticSystem(sigma=sigma, distr=type)
+    x = list(np.linspace(-math.pi, math.pi, M))
+    plt.plot(x, [s.m(xn) for xn in x])
+    plt.plot(x, s.mN(N, L, x))
+    plt.xlabel("x")
+    plt.legend(["Oryginał", "mN"])
+    plt.show()
+
+def Lab7_zad5(N: int, L: List[int], Q: int = 100, sigma: float = 0.1, type: str = "N"):
+    s = StaticSystem(sigma=sigma, distr=type)
+    plt.stem(L, s.valid(N=N, L=L, Q=Q))
+    plt.xlabel("L")
+    plt.ylabel("valid(L)")
+    plt.show()
 
 
+Lab7_zad2(N=1000)
+Lab7_zad4(N=250, M=100, L=15)
+Lab7_zad5(N=250, L=list(range(10)))
+
+Lab7_zad2(N=250, type="C")
+Lab7_zad4(N=250, M=100, L=15, type="C")
--- orto.py
+++ orto.py
@@ -2,34 +2,41 @@ import math
 import numpy as np
 from typing import List
 
-class StaticSystem():
-    def __init__(self, sigma: float = 1, a: float = 1):
-        self.sigma = abs(sigma)
-        self.a = a
 
-    def m(self, x: float) -> float:
+class StaticSystem:
+    def __init__(self, sigma: float = 1, distr: str = "N"):
+        self.sigma, self.distr = abs(sigma), distr
+
+    @staticmethod
+    def m(x: float) -> float:
+        x+=1
         if x > 2 or x < -2:
             return 0
-        if x <= 2 and x > 1 or x >= -2 and x < -1:
+        if 2 >= x > 1 or -2 <= x < -1:
             return 1
         else:
-            return self.a * x ** 2
+            return x ** 2
 
+    @staticmethod
+    def fi(k: int, x: float) -> float:
+        if k == 0:
+            return (2 * math.pi) ** (-1 / 2)
+        else:
+            return math.cos(k * x) * math.pi ** (-1 / 2)
 
     def simulate(self, N):
         Xn = list(np.random.uniform(-math.pi, math.pi, N))
-        Zn = list(np.random.normal(0, self.sigma, N))
+        if self.distr == "N":
+            Zn = list(np.random.normal(0, self.sigma, N))
+        elif self.distr == "C":
+            Zn = [self.sigma * math.tan(math.pi * (x - 0.5)) for x in np.random.uniform(0, 1, N)]
+        else:
+            raise Exception(f"{self.distr} not supported")
         Yn = [Zn[i] + self.m(Xn[i]) for i in range(N)]
         return Xn, Zn, Yn
 
 
-    def fi(self, k: int, x: float) -> float:
-        if k == 0:
-            return (2 * math.pi) ** (-1/2)
-        else:
-            return math.cos(k * x) * math.pi ** (-1/2)
-
-    def alfa_k(self,Xn: List[float], Yn: List[float], k: int) -> float:
+    def alfa_k(self, Xn: List[float], Yn: List[float], k: int) -> float:
         return float(np.mean(
             [Yn[i] * self.fi(k=k, x=x) for i, x in enumerate(Xn)]
         ))
@@ -39,20 +46,24 @@ class StaticSystem():
             [self.fi(k=k, x=x) for x in Xn]
         ))
 
-    def mN(self, N: int, L: int, X: List[float]):
+    def mN(self, N: int, L: int, X: List[float]) -> List[float]:
         Xn, Zn, Yn = self.simulate(N)
         mN = []
-
         for x in X:
             gN = sum([
-                self.alfa_k(Xn, Yn, k) * self.fi(k=k, x=x) for k in range(L+1)
-            ])
+                self.alfa_k(Xn, Yn, k) * self.fi(k=k, x=x) for k in range(L + 1)])
             fN = sum([
-                self.beta_k(Xn, k) * self.fi(k=k, x=x) for k in range(L + 1)
-            ])
+                self.beta_k(Xn, k) * self.fi(k=k, x=x) for k in range(L + 1)])
             if fN == 0:
                 mN.append(0)
             else:
-                mN.append(gN/fN)
-
+                mN.append(gN / fN)
         return mN
+
+    def valid(self, N: int, L: List[int], Q: int) -> List[float]:
+        qVector = [2 * q / Q for q in list(np.linspace(-Q, Q, 2 * Q + 1))]
+        output = []
+        for l in L:
+            mN = self.mN(N=N, L=l, X=qVector)
+            output.append(sum([(mN[i] - self.m(q)) ** 2 for i, q in enumerate(qVector)]) / (2 * Q))
+        return output
